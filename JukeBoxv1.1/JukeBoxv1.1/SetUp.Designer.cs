85ad6c5d6a3e569546db974b2b650fe0a9e6dde6
Miles <Miles@MSI>
2018-04-16 13:30:50 +0100

    Set up GUI done

---
+++ JukeBoxv1.1/JukeBoxv1.1/SetUp.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace JukeBoxv1._1
+{
+    partial class SetUp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.Import_lst = new System.Windows.Forms.ListBox();
+            this.Present_lst = new System.Windows.Forms.ListBox();
+            this.GenreaTitle_lst = new System.Windows.Forms.Label();
+            this.GenreTitle_txt = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button3);
+            this.groupBox1.Controls.Add(this.button2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(776, 300);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button5);
+            this.groupBox2.Controls.Add(this.button4);
+            this.groupBox2.Controls.Add(this.Import_lst);
+            this.groupBox2.Location = new System.Drawing.Point(6, 19);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 269);
+            this.groupBox2.TabIndex = 0;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Imported Tracks";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button9);
+            this.groupBox3.Controls.Add(this.button8);
+            this.groupBox3.Controls.Add(this.button7);
+            this.groupBox3.Controls.Add(this.button6);
+            this.groupBox3.Controls.Add(this.GenreTitle_txt);
+            this.groupBox3.Controls.Add(this.GenreaTitle_lst);
+            this.groupBox3.Controls.Add(this.Present_lst);
+            this.groupBox3.Location = new System.Drawing.Point(472, 19);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(298, 269);
+            this.groupBox3.TabIndex = 0;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Present Genre Track List";
+            // 
+            // Import_lst
+            // 
+            this.Import_lst.FormattingEnabled = true;
+            this.Import_lst.Location = new System.Drawing.Point(6, 19);
+            this.Import_lst.Name = "Import_lst";
+            this.Import_lst.Size = new System.Drawing.Size(242, 186);
+            this.Import_lst.TabIndex = 0;
+            // 
+            // Present_lst
+            // 
+            this.Present_lst.FormattingEnabled = true;
+            this.Present_lst.Location = new System.Drawing.Point(6, 58);
+            this.Present_lst.Name = "Present_lst";
+            this.Present_lst.Size = new System.Drawing.Size(286, 147);
+            this.Present_lst.TabIndex = 1;
+            // 
+            // GenreaTitle_lst
+            // 
+            this.GenreaTitle_lst.AutoSize = true;
+            this.GenreaTitle_lst.Location = new System.Drawing.Point(6, 19);
+            this.GenreaTitle_lst.Name = "GenreaTitle_lst";
+            this.GenreaTitle_lst.Size = new System.Drawing.Size(65, 13);
+            this.GenreaTitle_lst.TabIndex = 2;
+            this.GenreaTitle_lst.Text = "Genera Title";
+            // 
+            // GenreTitle_txt
+            // 
+            this.GenreTitle_txt.Location = new System.Drawing.Point(9, 35);
+            this.GenreTitle_txt.Name = "GenreTitle_txt";
+            this.GenreTitle_txt.Size = new System.Drawing.Size(283, 20);
+            this.GenreTitle_txt.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(292, 64);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Copy Track >>";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(292, 93);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 23);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Move Track >>";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(325, 202);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(87, 64);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Delete Track from Genera";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(6, 211);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(242, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Import Tracks from Directory";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(6, 240);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(242, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Clear Import Tracks";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(9, 211);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 52);
+            this.button6.TabIndex = 7;
+            this.button6.Text = "<< Previous";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(208, 211);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(84, 52);
+            this.button7.TabIndex = 8;
+            this.button7.Text = "Next >>";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(112, 211);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(75, 23);
+            this.button8.TabIndex = 9;
+            this.button8.Text = "Add";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(112, 240);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(75, 23);
+            this.button9.TabIndex = 10;
+            this.button9.Text = "Delete";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(606, 318);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 7;
+            this.button10.Text = "OK";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(687, 318);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(75, 23);
+            this.button11.TabIndex = 8;
+            this.button11.Text = "CANCEL";
+            this.button11.UseVisualStyleBackColor = true;
+            // 
+            // SetUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 348);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "SetUp";
+            this.Text = "SetUp";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox GenreTitle_txt;
+        private System.Windows.Forms.Label GenreaTitle_lst;
+        private System.Windows.Forms.ListBox Present_lst;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ListBox Import_lst;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
